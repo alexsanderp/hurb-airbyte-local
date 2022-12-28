72e70780b0d86e467e4ee6d21cf0040773b21d67
Alexsander Pereira <alexsander.pereira@icloud.com>
2022-12-28 18:59:32 -0300

    FIX: FIX release_name

--- scripts/run_local_airbyte.sh
+++ scripts/run_local_airbyte.sh
@@ -5,12 +5,12 @@ sudo echo ""
 minikube start --memory=4000
 
 # install airbyte
-helm install local airbyte/airbyte
+helm install airbyte airbyte/airbyte
 
 # forwarding a port from airbyte to local port 8000
 RET=1
 until [ ${RET} -eq 0 ]; do
-    kubectl port-forward svc/local-airbyte-webapp-svc 8000:80
+    kubectl port-forward svc/airbyte-airbyte-webapp-svc 8000:80
     RET=$?
     sleep 10
 done
